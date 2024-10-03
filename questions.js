65bc5e6a6b87e2c72a53a2568f86f2594509076c
Mohammed Ansar Hussain <77661525+Dev-Ansar@users.noreply.github.com>
2024-10-02 21:25:16 -0500

    initial commit
    initial commit

---
+++ questions.js
@@ -0,0 +1,65 @@
+const questions = [
+    {
+        question: "What is the name of Thor's hammer?",
+        options: ["Jonothan", "Gungnir", "Mjolnir", "Stormbreaker"],
+        answer: "Mjolnir",
+        hint: "It's enchanted with a spell that allows only those who are 'worthy' to lift it."
+    },
+    {
+        question: "What is the real name of Iron Man?",
+        options: ["Bruce Wayne", "Tony Stark", "Steve Rogers", "Clark Kent"],
+        answer: "Tony Stark",
+        hint: "He is a genius billionaire playboy philanthropist."
+    },
+    {
+        question: "Who is Thor's adoptive brother?",
+        options: ["Loki", "Heimdall", "Odin", "Hela"],
+        answer: "Loki",
+        hint: "He is known as the God of Mischief."
+    },
+    {
+        question: "What is the name of the Black Panther's home country?",
+        options: ["Atlantis", "Wakanda", "Genosha", "Latveria"],
+        answer: "Wakanda",
+        hint: "It is known for its advanced technology and vibranium."
+    },
+    {
+        question: "Which character wields the shield made of vibranium?",
+        options: ["Bucky Barnes", "Steve Rogers", "Sam Wilson", "Peggy Carter"],
+        answer: "Steve Rogers",
+        hint: "He is also known as Captain America."
+    },
+    {
+        question: "In the MCU, who was the first character to say, 'I am Iron Man'?",
+        options: ["Tony Stark", "Nick Fury", "Rhodey", "Happy Hogan"],
+        answer: "Tony Stark",
+        hint: "He said this at the end of the first Iron Man movie."
+    },
+    {
+        question: "Which Marvel superhero is known as the 'Merc with a Mouth'?",
+        options: ["Spider-Man", "Deadpool", "Wolverine", "The Punisher"],
+        answer: "Deadpool",
+        hint: "He is known for breaking the fourth wall."
+    },
+    {
+        question: "Who is the ruler of Asgard in the Marvel Universe?",
+        options: ["Loki", "Thor", "Odin", "Hela"],
+        answer: "Odin",
+        hint: "He is the All-Father and father of Thor."
+    },
+    {
+        question: "Who is the first female superhero in the Marvel Cinematic Universe to have her own solo film?",
+        options: ["Black Widow", "Scarlet Witch", "Captain Marvel", "Gamora"],
+        answer: "Captain Marvel",
+        hint: "She is portrayed by Brie Larson."
+    },
+    {
+        question: "What is the name of the cosmic entity that the Avengers fight in 'Avengers: Infinity War'?",
+        options: ["Ultron", "Thanos", "Loki", "Kang"],
+        answer: "Thanos",
+        hint: "He seeks the Infinity Stones to wipe out half of all life."
+    }
+
+];
+
+// You can now use the `marvelTriviaQuestions` array in your quiz logic.
